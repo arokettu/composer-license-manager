2303deacf8dae96c816cd870e5a67a99b28fdba8
Anton Smirnov <sandfox@sandfox.me>
2023-08-18 22:00:51 +0300

    Update docs from template

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,10 @@
 from datetime import datetime
 
-project = 'Composer License Manager'
+# import specific project config
+import os, sys
+sys.path.append(os.curdir)
+from conf_project import *
+
 author = 'Anton Smirnov'
 copyright = '{} {}'.format(datetime.now().year, author)
 language = 'en'
@@ -8,3 +12,15 @@ language = 'en'
 html_title = project
 html_theme = 'furo'
 templates_path = ["_templates"]
+html_sidebars = {
+    "**": [
+        "sidebar/brand.html",
+        "rtd-version.html",
+        "sidebar/search.html",
+        "sidebar/scroll-start.html",
+        "sidebar/navigation.html",
+        "sidebar/ethical-ads.html",
+        "sidebar/scroll-end.html",
+        "sidebar/variant-selector.html",
+    ]
+}
